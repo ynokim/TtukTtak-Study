d429edf9389e1f5dfaf8c8412a0ebfd1fd613584
ynokim <i_am@ynokim.com>
2021-12-21 20:02:02 +0900

    [hashset] fixed error

--- hashset/test.py
+++ hashset/test.py
@@ -3,17 +3,17 @@
 
 import hashlib
 import os
-from PIL import Image
 
 img_list = []
 count = 0
 count_hash = 0
 
-#input hash file
+#input hash file: txt 파일로 미리 정의된 md5 해시 목록을 불러온다.
 input_filename = input("Input Hash File Name: ")
 input_filename += ".txt"
 input_take = open(input_filename, 'r')
 
+#txt 파일에  있는 md5 해시 목록을 읽고 정리한다.
 while True:
     line = input_take.readline()
     line = line.strip()
@@ -24,11 +24,11 @@ while True:
     img_list[count] = line
     count += 1
 
-#input directory
+#input directory: 비교할 파일들이 있는 디렉토리를 절대경로로 넣는다.
 find_directory = input("Input Directory you want to find: ")
 file_list = os.listdir(find_directory)
 
-#check md5 match
+#check md5 match: 두 파일의 md5 해시를 비교하여 일치할 시 1, 일치하지 않을 시 0을 return함
 def compare_md5(text, count):
     global count_hash
     for j in range(0, count):
@@ -39,14 +39,11 @@ def compare_md5(text, count):
             pass
     return 0
 
-#compare md5 hash
+#compare md5 hash: 두 파일의 md5 함수를 비교한다.
 for repeat in range(len(file_list)):
     with open(find_directory + '/' + file_list[repeat], 'rb') as file_take:
         file_reader = file_take.read(4096)
         #print(file_reader)
-
-        extension = os.path.splitext()
-
         enc = hashlib.md5()
         enc.update(file_reader)
         enc_text = enc.hexdigest()
@@ -55,5 +52,5 @@ for repeat in range(len(file_list)):
     if check:
         print(" %s'S HASH [%s] IS MATCHED: [%s] " % (file_list[repeat], enc_text, img_list[count_hash]))
     else:
-        print(" %s'S HASH [%s] IS NOT MATCHED: [%s] " % (file_list[repeat], enc_text, img_list[count_hash]))
+        print(" %s'S HASH [%s] IS NOT MATCHED" % (file_list[repeat], enc_text))
         pass
