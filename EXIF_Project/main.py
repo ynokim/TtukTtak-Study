b7fa757d5defe383e76aca2898fbb08ef8ad7082
ynokim <i_am@ynokim.com>
2021-12-24 17:25:21 +0900

    [EXIF_Parsing] Add Kakao Map

--- EXIF_Project/main.py
+++ EXIF_Project/main.py
@@ -1,12 +1,11 @@
 from exif import Image
 import webbrowser
 
+# Open File
 with open("20211221_125748.jpg", "rb") as file_read:
     image = Image(file_read)
 
-def format_dms_coordinates(coordinates):
-    return f"{coordinates[0]}° {coordinates[1]}\' {coordinates[2]}\""
-
+# 위도와 경도
 def dms_coordinates_to_dd_coordinates(coordinates, coordinates_ref):
     decimal_degrees = coordinates[0] + coordinates[1] / 60 + coordinates[2] / 3600
     if coordinates_ref == "S" or coordinates_ref == "W":
@@ -21,6 +20,7 @@ if image.has_exif:
     latitude = dms_coordinates_to_dd_coordinates(image.gps_latitude, image.gps_latitude_ref)
     longitude = dms_coordinates_to_dd_coordinates(image.gps_longitude, image.gps_longitude_ref)
     google_map = f"https://www.google.com/maps?q={latitude},{longitude}"
+    kakao_map = f"https://map.kakao.com/?q={latitude},{longitude}"
 
 print(f"image {status}")
 print("=============================")
@@ -29,4 +29,5 @@ print(f"{model}")
 print(f"위도(Decimal_degrees): {latitude}")
 print(f"경도(Decimal_degrees): {longitude}")
 print(f"Google Maps URL Link: {google_map}")
-webbrowser.open_new_tab(google_map)
+print(f"Kakao Map URL Link: {kakao_map}")
+webbrowser.open_new_tab(kakao_map)
