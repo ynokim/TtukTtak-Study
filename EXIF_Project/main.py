ededd91038ca753c00b87a9fb077bb4c8c5f3205
ynokim <i_am@ynokim.com>
2021-12-24 17:12:11 +0900

    [EXIF_Parsing] Upload Code

---
+++ EXIF_Project/main.py
@@ -0,0 +1,32 @@
+from exif import Image
+import webbrowser
+
+with open("20211221_125748.jpg", "rb") as file_read:
+    image = Image(file_read)
+
+def format_dms_coordinates(coordinates):
+    return f"{coordinates[0]}° {coordinates[1]}\' {coordinates[2]}\""
+
+def dms_coordinates_to_dd_coordinates(coordinates, coordinates_ref):
+    decimal_degrees = coordinates[0] + coordinates[1] / 60 + coordinates[2] / 3600
+    if coordinates_ref == "S" or coordinates_ref == "W":
+        decimal_degrees = -decimal_degrees
+
+    return decimal_degrees
+
+if image.has_exif:
+    status = f"contains EXIF (version {image.exif_version}) information."
+    maker = f"Camera manufacturer: {image.make}"
+    model = f"Camera model: {image.model}"
+    latitude = dms_coordinates_to_dd_coordinates(image.gps_latitude, image.gps_latitude_ref)
+    longitude = dms_coordinates_to_dd_coordinates(image.gps_longitude, image.gps_longitude_ref)
+    google_map = f"https://www.google.com/maps?q={latitude},{longitude}"
+
+print(f"image {status}")
+print("=============================")
+print(f"{maker}")
+print(f"{model}")
+print(f"위도(Decimal_degrees): {latitude}")
+print(f"경도(Decimal_degrees): {longitude}")
+print(f"Google Maps URL Link: {google_map}")
+webbrowser.open_new_tab(google_map)
