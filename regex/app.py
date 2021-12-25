88898aa1e9fa84fb26088c21441f4e37a3cb957d
ynokim <i_am@ynokim.com>
2021-12-25 15:46:52 +0900

    [regex] add comments

--- regex/app.py
+++ regex/app.py
@@ -4,10 +4,12 @@ text_list = []
 address_list = []
 count = 0
 
+# 맨 앞의 두 글자로 코인의 주소 특정
 btc_pattern = r"^bc"
 eth_pattern = r"^0x"
 rip_pattern = r"^rU"
 
+# address.txt 파일에 있는 원본 주소 리스트에 추가
 with open("address.txt", "r") as file:
     while True:
         text_line = file.readline()
@@ -18,6 +20,7 @@ with open("address.txt", "r") as file:
             break
         count += 1
 
+# address.txt 파일에서 주소를 특정하고 특정 주소가 일치할 시 그 주소의 코인 이름 리스트에 추가
 with open("address.txt", "r") as file:
     for line in file:
         if re.search(btc_pattern, line):
@@ -40,6 +43,7 @@ with open("address.txt", "r") as file:
             address_list.append(output_nothing)
             pass
 
+    # 결과갑 출력
     for i in range(0, count):
         print("%s 주소의 코인은 다음과 같습니다: %s" % (text_list[i], address_list[i]))
 
